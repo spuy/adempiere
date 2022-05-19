b70df74ba296d4b9a5804aaac665aa8e0828f0ae
Carlos Parada <carlosaparadam@gmail.com>
2022-05-18 21:30:42 -0400

    Remove unused commands on setting script and docker compose

--- docker/adempiere/setting.sh
+++ docker/adempiere/setting.sh
@@ -9,8 +9,6 @@ cd /opt/binaryapp/
 for d in */ ; do [ -L "${d%/}" ] && continue; mv "/opt/binaryapp/$d"* /opt/binaryapp/ ; done
 echo "ADempiere Home..." 
 cd $ADEMPIERE_HOME
-#chmod -Rf 755 *.sh 
-#chmod -Rf 755 utils/*.sh 
 cp AdempiereEnvTemplate.properties AdempiereEnv.properties
 sed -i "s|JAVA_HOME=/usr/lib/jvm/jdk-11|JAVA_HOME=$JAVA_HOME|g" AdempiereEnv.properties 
 sed -i "s|ADEMPIERE_HOME=C.*|ADEMPIERE_HOME=$ADEMPIERE_HOME|g" AdempiereEnv.properties
